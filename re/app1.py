2495c992a1e073e05c8f66c90a489b53fa38a547
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2020-06-13 21:56:50 +0530

    added the files

---
+++ re/app1.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+import numpy
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    ### Generating X,Y coordinaltes to be used in plot
+    nuumpy.load('C:\\Users\\Ravitha\\Downloads\train')
+    X = numpy.linspace(0,10,30)
+    Y = X*X
+    ### Generating The Plot
+    plt.plot(X,Y)
+    ### Saving plot to disk in png format
+    #plt.savefig('/home/AnandSatya/mysite/square_plot.png')
+
+    ### Rendering Plot in Html
+    figfile = BytesIO()
+    plt.savefig(figfile, format='png')
+    figfile.seek(0)
+    figdata_png = base64.b64encode(figfile.getvalue()).decode('ascii')
+    result = figdata_png
+    return render_template('gallery1.html', result=result)
+
+if __name__ == "__main__":
+    app.run(port=4555, debug=True)
