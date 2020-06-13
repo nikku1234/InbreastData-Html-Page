bf0943e8426b44e05795d2d892f99be47b377725
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2020-06-14 04:25:11 +0530

    working code

--- flask-app/generateArray-to-image-to-arrayBack.py
+++ flask-app/generateArray-to-image-to-arrayBack.py
@@ -2,46 +2,49 @@ import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib
 
-from PIL import Image                                                                                
+from PIL import Image       
 
-
-####create a matrix of random colors
 filename = "example"
 
-matrix=np.random.random((4,4,3))
-nx,ny,nz=np.shape(matrix)
-CXY=np.zeros([ny, nx])
-for i in range(ny):
-    for j in range(nx):
-        CXY[i,j]=np.max(matrix[j,i,:])
 
-#Save binary data
-np.save(filename + '.npy', CXY)
-print(filename + " was saved")
+##testing with a random generated image
+# ####create a matrix of random colors
+# filename = "new_file"
 
-#Load npy
-img_array = np.load(filename + '.npy')
-plt.imshow(img_array)
+# matrix=np.random.random((4,4,3))
+# nx,ny,nz=np.shape(matrix)
+# CXY=np.zeros([ny, nx])
+# for i in range(ny):
+#     for j in range(nx):
+#         CXY[i,j]=np.max(matrix[j,i,:])
 
+# #Save binary data
+# np.save(filename + '.npy', CXY)
+# print(filename + " was saved")
 
-####Save npy as png
-filename = "example"
-
-img_name = filename +".png"
-matplotlib.image.imsave(img_name, img_array)
-print(filename + " was saved")
+# #Load npy
+# img_array = np.load(filename + '.npy')
+# plt.imshow(img_array)
 
 
-#### Convert that png back to numpy array
+# ####Save npy as png
+# filename = "new_file"
 
-img = Image.open( filename + '.png' )
-data = np.array( img, dtype='uint8' )
+# img_name = filename +".png"
+# matplotlib.image.imsave(img_name, img_array)
+# print(filename + " was saved")
 
-#Convert the new npy file to png
-filename = "new-array"
 
-np.save( filename + '.npy', data)
-print(filename + " was saved")
+# #### Convert that png back to numpy array
+
+# img = Image.open( filename + '.png' )
+# data = np.array( img, dtype='uint8' )
+
+# #Convert the new npy file to png
+# filename = "new-array"
+
+# np.save( filename + '.npy', data)
+# print(filename + " was saved")
 
 
 #Load npy
