94f4e76f9e4bc44b12e4a007359eb4e4c21deddd
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2020-06-14 19:31:39 +0530

    computation code and working

---
+++ flask-app/computation_files/computation.py
@@ -0,0 +1,19 @@
+import numpy
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
+### Generating X,Y coordinaltes to be used in plot
+numpy.load('../Inbreastdata.npy')
+X = numpy.linspace(0,10,30)
+Y = X*X
+### Generating The Plot
+plt.plot(X,Y)
+### Saving plot to disk in png format
+plt.savefig('plt.png')
+
+### Rendering Plot in Html
+figfile = BytesIO()
+plt.savefig(figfile, format='png')
+figfile.seek(0)
+figdata_png = base64.b64encode(figfile.getvalue()).decode('ascii')
+result = figdata_png
