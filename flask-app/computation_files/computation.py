860b3433cddfc91d53e4dee857a834af89b893db
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2020-06-14 21:14:45 +0530

    code working

--- flask-app/computation_files/computation.py
+++ flask-app/computation_files/computation.py
@@ -1,13 +1,28 @@
 import numpy
+from numpy import savetxt
 import matplotlib.pyplot as plt
+import matplotlib
 from io import BytesIO
 import base64
+from PIL import Image   
+
 ### Generating X,Y coordinaltes to be used in plot
 data = numpy.load('../Inbreastdata.npy')
 print(type(data))
 print(len(data))
 print(data.shape)
-print(data)
+print(data[0])
+size= len(data)
+print(size)
+filename = "new-image"
+#Save as png
+img_name = filename +".png"
+matplotlib.image.imsave(img_name, data[0])
+print(filename + " was saved")
+
+
+
+# savetxt('data.csv', data, delimiter=',')
 # numpy.savetxt('data.txt',data, delimiter=' ')
 X = numpy.linspace(0,10,30)
 Y = X*X
