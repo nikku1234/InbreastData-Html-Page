bfbc8d2659c355e1ebbb1dc16e24086982b3bfae
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2020-06-18 12:29:38 +0530

    file structure

--- new-app/app.py
+++ new-app/app.py
@@ -20,9 +20,9 @@ import base64
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def hello():
     ### Generating X,Y coordinaltes to be used in plot
-    numpy.load('../Inbreastdata.npy')
+    numpy.load('Inbreastdata.npy')
     X = numpy.linspace(0,10,30)
     Y = X*X
     ### Generating The Plot
@@ -39,4 +39,4 @@ def hello_world():
     return render_template('gallery1.html', result=result)
 
 if __name__ == "__main__":
-    app.run(port=4555, debug=True)
+    app.run()
