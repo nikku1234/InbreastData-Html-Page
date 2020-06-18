6f8c89ca08d9f59a34e3024490e4b4f4aec291f6
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2020-06-18 13:21:31 +0530

    app

--- new-app/app.py
+++ new-app/app.py
@@ -15,6 +15,7 @@ import numpy
 import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
+import matplotlib
 
 
 app = Flask(__name__)
@@ -23,20 +24,26 @@ app = Flask(__name__)
 def hello():
     ### Generating X,Y coordinaltes to be used in plot
     numpy.load('Inbreastdata.npy')
-    X = numpy.linspace(0,10,30)
-    Y = X*X
+    # X = numpy.linspace(0,10,30)
+    # Y = X*X
     ### Generating The Plot
-    plt.plot(X,Y)
+    # plt.plot(X,Y)
     ### Saving plot to disk in png format
     #plt.savefig('/home/AnandSatya/mysite/square_plot.png')
+    for i in range(size):
+        filename = "image"+str(i)
+        img_name = filename +".png"
+        matplotlib.image.imsave("images/"+img_name, data[i],cmap="gray")
+        print(filename + " was saved")
 
-    ### Rendering Plot in Html
-    figfile = BytesIO()
-    plt.savefig(figfile, format='png')
-    figfile.seek(0)
-    figdata_png = base64.b64encode(figfile.getvalue()).decode('ascii')
-    result = figdata_png
-    return render_template('gallery1.html', result=result)
+    # ### Rendering Plot in Html
+    # figfile = BytesIO()
+    # plt.savefig(figfile, format='png')
+    # figfile.seek(0)
+    # figdata_png = base64.b64encode(figfile.getvalue()).decode('ascii')
+    # result = figdata_png
+    # return render_template('gallery1.html', result=result)
+    return "Hello World!"
 
 if __name__ == "__main__":
     app.run()
